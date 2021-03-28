42c4be79ed4834b7d1ffd464053d9890bc621406
erbridge <f@erbridge.co.uk>
2021-03-28 02:14:05 +0200

    Only run if the context has a pull request

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const core = require("@actions/core");
+const github = require("@actions/github");
+const yn = require("yn");
+const validate = require("./src/validate");
 
 async function run() {
   try {
+    {
+      const strict = yn(core.getInput("strict"));
+
+      const { warning, failure } = validate(github.context.payload, strict);
+
+      if (failure) {
+        throw new Error(failure);
+      }
+
+      if (warning) {
+        core.warning(warning);
+        return;
+      }
+    }
+
     core.info("Running...");
   } catch (error) {
     core.setFailed(error.message);
